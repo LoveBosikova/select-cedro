b6121beb3a127e5470fc75262cca3578cea4f04e
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-08-26 00:55:33 +0300

    styles

--- select/src/App.tsx
+++ select/src/App.tsx
@@ -3,8 +3,14 @@ import './App.scss'
 function App() {
 
   return (
-    <>
-    </>
+    <main className='main'>
+      <div className='contentWrap'>
+        <section className='select'></section>
+        <section className='select--actionSheet'></section>
+        <section className='select--multi'></section>
+        <section className='select--combobox'></section>
+      </div>
+    </main>
   )
 }
 
